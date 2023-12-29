9eec7c501a4eb61af59bf3d519e70029b3b4066e
Angelina <angelina.kulyaeva@yandex.ru>
2023-12-29 19:25:13 +0300

    Added main layout

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -15,11 +15,71 @@
     header.append(headerContainer);
 
     header.headerContainer = headerContainer;
+
+    return header;
+  };
+
+  const createLogo = title => {
+    const h1 = document.createElement('h1');
+    h1.classList.add('logo');
+    h1.textContent = `Телефонный справочник. ${title}`;
+
+    return h1;
+  };
+
+  const createMain = () => {
+    const main = document.createElement('main');
+    const mainContainer = createContainer();
+    main.append(mainContainer);
+    main.mainContainer = mainContainer;
+
+    return main;
+  };
+
+  const createButtonsGroup = params => {
+    const btnWrapper = document.createElement('div');
+    btnWrapper.classList.add('btn-wrapper');
+
+    const btns = params.map(({className, type, text}) => {
+      const button = document.createElement('button');
+      button.type = type;
+      button.textContent = text;
+      button.className = className;
+
+      return button;
+    });
+
+    btnWrapper.append(...btns);
+
+    return {
+      btnWrapper,
+      btns,
+    };
   };
 
   const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
     const header = createHeader();
+    const logo = createLogo();
+    const main = createMain();
+    const buttonGroup = createButtonsGroup([
+      {
+        className: 'btn btn-primary mr-3',
+        type: 'button',
+        text: 'Добавить',
+      },
+      {
+        className: 'btn btn-danger',
+        type: 'button',
+        text: 'Удалить',
+      },
+    ]);
+
+    header.headerContainer.append(logo);
+
+    main.mainContainer.append(buttonGroup.btnWrapper);
+
+    app.append(header, main);
   };
 
   window.phonebookInit = init;
